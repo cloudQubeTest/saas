2d90ef9aadd64ea19d6ba0664ff0b4704043b614
caohongd <amesatchu2014@gmail.com>
2017-04-30 11:03:26 -0700

    Added Model folder for Mongoose Schemas and Models

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
+//module dependencies
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose= require('mongoose');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+//all environments
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.logger('dev'));
+app.use(bodyParser.json()); //possibly comment out
+app.use(bodyParser.urlencoded({ extended: false })); //possibly comment out
 app.use(cookieParser());
+app.use(express.favicon());
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+
+//call mongoose connect to connect to local database here
+//mongoose.connect(/*database here*/);
+
+
 
 app.use('/', index);
 app.use('/users', users);
